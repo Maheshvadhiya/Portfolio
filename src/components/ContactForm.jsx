5ce838d0059fa7b94c929b2af84de162ae1d4e1b
Maheshvadhiya <maheshvadhiya47@gmail.com>
2024-07-04 14:39:47 +0530

    add style

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -75,17 +75,18 @@ const ContactForm = () => {
       <Form noValidate validated={isValidated} onSubmit={handleSubmit}>
         <Form.Group className="mx-auto mb-3 form-group" controlId="name">
           <Form.Label>Name</Form.Label>
-          <Form.Control required type="text" placeholder="Your name" />
+          <Form.Control required type="text" placeholder="Your name" style={{ height:40 }} />
           <Form.Control.Feedback type="invalid">
             <h6>Name must be at least one character.</h6>
           </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mx-auto mb-3 form-group" controlId="email">
+        <Form.Group className="mx-auto mb-3 form-group" controlId="email" >
           <Form.Label>Email address</Form.Label>
           <Form.Control
             required
             pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
             placeholder="someone@something.com"
+            style={{ height:40 }}
           />
           <Form.Control.Feedback type="invalid">
             <h6>Please enter a valid email.</h6>
@@ -93,7 +94,7 @@ const ContactForm = () => {
         </Form.Group>
         <Form.Group className="mx-auto mb-3 form-group" controlId="message">
           <Form.Label>Message</Form.Label>
-          <Form.Control required as="textarea" placeholder="Your message..." />
+          <Form.Control required as="textarea" placeholder="Your message..." style={{ height:80 }}/>
           <Form.Control.Feedback type="invalid">
             <h6>Please provide a valid message.</h6>
           </Form.Control.Feedback>
